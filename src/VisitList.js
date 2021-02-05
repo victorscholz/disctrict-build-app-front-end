34dbe529737c1dbf0c1c9385a727991e0535e017
victorscholz <vgscholz@gmail.com>
2021-02-05 13:44:18 -0500

    added pointer to view history, changed menu to onClick

--- src/VisitList.js
+++ src/VisitList.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import VisitListCard from "./VisitListCard";
 
 function VisitList(props) {
   const districtBuildButton = <h2>DistrictBuild NYC</h2>;
+  const [hovered, setHover] = useState(false);
+  useEffect(() => {
+    document.body.style.cursor = hovered ? "pointer" : "auto";
+  }, [hovered]);
 
   const visitListCards = props.visitListArray.map((building) => (
     <VisitListCard
@@ -14,8 +18,14 @@ function VisitList(props) {
 
   return (
     <div
-      onMouseEnter={props.changeHistoryState}
-      onMouseLeave={props.changeHistoryState}
+      onPointerOver={(e) => {
+        setHover(true);
+      }}
+      onPointerOut={(e) => {
+        setHover(false);
+      }}
+      onClick={props.changeHistoryState}
+      // onMouseLeave={props.changeHistoryState}
       className={props.historyClicked ? "visitList" : "sidebarStyle"}
     >
       {props.historyClicked ? <strong>View History</strong> : null}
